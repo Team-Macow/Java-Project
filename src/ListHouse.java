a0834b0aeda665e8cd6cc76c8881418881808bf4
Dilani <dilani_f@ymail.com>
2015-04-03 15:38:38 +0530

    Implemented all the methods in the ListHouse.java class. Corrected the errors. This class holds the information about a specific house.

--- src/ListHouse.java
+++ src/ListHouse.java
@@ -6,8 +6,83 @@
 
 /**
  *
- * @author Dell
+ * @author Dilani
  */
-public class ListHouse {
+
+public class ListHouse implements Listable{
+    
+     
+    private String lastName;
+    private String firstName;
+    private int lotNumber;
+    private int price;
+    private int squareFeet;
+    private int bedRooms;
+    
+    public ListHouse(String lastName, String firstName, int lotNumber,
+		int price, int squareFeet, int bedRooms )
+		{
+		this.lastName = lastName;
+		this.firstName = firstName;
+		this.lotNumber = lotNumber;
+		this.price = price;
+		this.squareFeet = squareFeet;
+		this.bedRooms = bedRooms;
+		}
+    
+                public Listable copy()
+		
+		{
+		ListHouse result = new ListHouse(lastName, firstName, lotNumber, price,
+		squareFeet, bedRooms);
+		return result;
+		}
+                
+                public String compareTo(Listable comp)
+		{
+			int result;
+		ListHouse other = (ListHouse)comp;
+		 result = this.lotNumber - other.lotNumber;
+		
+		if (result >0) {
+			
+			return "Greater";
+			
+		} else if (result <0) {
+			
+			return "Less";
+	
+		} else {
+			
+			return "Equal";
+			
+		} 		 
+		
+		}
+                
+                public String lastName()
+		{
+		return lastName;
+		}
+		public String firstName()
+		{
+		return firstName;
+		}
+		public int lotNumber()
+		{
+		return lotNumber;
+		}
+		public int price()
+		{
+		return price;
+		}
+		public int squareFeet()
+		{
+		return squareFeet;
+		}
+		public int bedRooms()
+		{
+		return bedRooms;
+		}
     
 }
